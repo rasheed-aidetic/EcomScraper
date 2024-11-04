344a3ab476b8c2d11972c11734e05d8e2cfbead3
rasheed-aidetic <abdul.rasheed@aidetic.in>
2024-11-04 17:14:04 +0530

    added extra fields in db and modified save file

--- config.py
+++ config.py
@@ -1,9 +1,26 @@
 import os
 
 # config.py
-WEBSITES = [
-    "https://www.mcaffeine.com"
-    # Add other URLs here
+WEBSITES =[
+    "https://www.mcaffeine.com/",
+    "https://www.svarasya.com/",
+    "https://beminimalist.co/",
+    "https://letshyphen.com/",
+    "https://plumgoodness.com/",
+    "https://foxtale.in/",
+    "https://aqualogica.in/",
+    "https://consciouschemist.com/",
+    "https://mycosiq.com/",
+    "https://www.nuutjob.com/",
+    "https://isakfragrances.com/",
+    "https://www.oddgiraffe.com/",
+    "https://www.organicsmokes.co/",
+    "https://nirmalaya.com/",
+    "https://hoovufresh.com/",
+    "https://bambooindia.com/",
+    "https://www.modernmyth.in/",
+    "https://www.gharsoaps.shop/",
+    "https://perforacare.com/"
 ]
 
 DB_PATH = os.environ.get("SQLITE_DB_PATH")
